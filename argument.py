d2ddc8c4a6cfeecdd18034841dd083dd51002c08
rokurosatp <a-cclemon-mesopota-root36@live.jp>
2017-08-22 12:35:02 +0900

    to_tuple（復号用）を追加

--- argument.py
+++ argument.py
@@ -11,6 +11,14 @@ class PredicateArgument:
         self.case = args[4]
         self.label = args[5]
         self.probable =  args[6]
+    def to_tuple(self):
+        """タプルに変換
+        """
+        return (
+            self.ana_sid, self.ana_tid,
+            self.ant_sid, self.ant_tid, self.case,
+            self.label, self.probable
+        )
     def get_repr(self, doc):
         return '{0} -{1:4}-> {2}: '.format(
             doc.refer(self.ana_ref()).get_surface(), self.case, doc.refer(self.ant_ref()).get_surface()
@@ -19,6 +27,7 @@ class PredicateArgument:
         return nlelement.TokenReference(self.ana_sid, self.ana_tid)
     def ant_ref(self):
         return nlelement.TokenReference(self.ant_sid, self.ant_tid)
+
 class CoreferenceArgument:
     def __init__(self, *args):
         self.ana_sid = args[0]
@@ -27,6 +36,14 @@ class CoreferenceArgument:
         self.ant_tid = args[3]
         self.label = args[4]
         self.probable =  args[5]
+    def to_tuple(self):
+        """タプルに変換
+        """
+        return (
+            self.ana_sid, self.ana_tid,
+            self.ant_sid, self.ant_tid,
+            self.label, self.probable
+        )
     def get_repr(self, doc):
         return '{0} ------> {1}: '.format(
             doc.refer(self.ana_ref()).get_surface(), doc.refer(self.ant_ref()).get_surface()
