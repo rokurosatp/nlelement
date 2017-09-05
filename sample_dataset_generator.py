1e09f3a8b95d7ff957867cf0fdb021a3f7206655
RokuroSatp <a-cclemon-mesopota-root36@live.jp>
2017-09-05 20:55:31 +0900

    デバッグ用データセット自動生成機能

---
+++ sample_dataset_generator.py
@@ -0,0 +1,26 @@
+"""サンプル用のデータセット（db）を生成する
+"""
+from nlelement.external import synchacall
+from nlelement import nlelement, database, cabocha_extended
+
+raw_samples = [
+"""このサンプルはコーパスを正しくロードできているか確認をするためのテストデータです。
+述語項構造解析ツール新茶で解析を行ったデータをデータベースに保存してください。
+データは生のまま使用する予定です。
+""",
+"""太郎はハワイに行った。
+そこにはすでに二郎がいた。
+花子はその知らせを聞き、急遽駆け付けた。
+太郎は引き続きハワイ旅行を楽しんでいる。
+"""
+]
+
+def main():
+    app = synchacall.AppCall('predicate/external/syncha-0.3.1.1/syncha')
+    docs = []
+    for raw_doc in raw_samples:
+        result_str = app.run('-I 0 -O 2 -u {}'.format('0'), raw_doc.rstrip("\n\r")+"\nEOT\n", throws=False)
+        doc = cabocha_extended.load_from_text(result_str, as_label=True)
+        docs.append(doc)
+    sample_db = database.DatabaseLoader('dat/sample_corpus.db')
+    sample_db.save(docs)
