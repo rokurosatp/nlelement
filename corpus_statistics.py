21c52a0bfe986119332be8ed5df2a50e85312e23
RokuroSatp <a-cclemon-mesopota-root36@live.jp>
2017-09-07 23:04:05 +0900

    気まぐれに-O 2に変更

--- corpus_statistics.py
+++ corpus_statistics.py
@@ -164,7 +164,7 @@ def count_syncha_stat():
     import subprocess
     from nlelement import cabocha_extended
     stats = PredicateStatTable()
-    process = subprocess.Popen(['./predicate/external/syncha-0.3.1.1/syncha', '-I', '2', '-k'], stdin=subprocess.PIPE)
+    process = subprocess.Popen(['./predicate/external/syncha-0.3.1.1/syncha', '-I', '2', '-O', '2',, '-k'], stdin=subprocess.PIPE)
     db = database.DatabaseLoader(bccwj.get_corpus_path())
     with open('./dat/log/stat_input.log', 'w') as f:
         for doc in db.load_as_iter():
