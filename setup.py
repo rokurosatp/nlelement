70592b8cbe4156aa0fb3988d29f50f1a5a3b73ef
rokurosatp <a-cclemon-mesopota-root36@live.jp>
2018-03-09 16:19:52 +0900

    [CHANGE]SQLスクリプトの取り込み

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='slackbotpry',
+    name='nlelement',
     version='0.0.1',
     packages=find_packages(exclude=['tests']),
     author='rokurosatp',
     url='https://github.com/rokurosatp/nlelement',
+    package_data={'nlelement': ['sqlcode/*.sql']},
     install_requires=['pydot-ng>=1.0.0',],
 )
